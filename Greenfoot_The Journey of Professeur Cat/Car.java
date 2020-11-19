fa1e93a773dc1ef51302e80e4564bbb812dcd302
AtozZone <astroatoz@gmail.com>
2020-11-18 19:46:33 -0500

    Added some images

--- Greenfoot_The Journey of Professeur Cat/Car.java
+++ Greenfoot_The Journey of Professeur Cat/Car.java
@@ -8,16 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Car extends Actor
 {
-    public int delta = -5;
     /**
      * Act - do whatever the Car wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        setLocation(getX(), getY() + delta); 
-        if (isAtEdge()) {
-            getWorld().removeObject(this); 
-        }
-    } 
+        // Add your action code here.
+    }    
 }
