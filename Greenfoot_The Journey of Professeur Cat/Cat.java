46a2a7905925d2caea337dce3fb7158a990d029d
Astour <project@gmail.com>
2020-11-27 18:12:20 -0500

    added the first level

---
+++ Greenfoot_The Journey of Professeur Cat/Cat.java
@@ -0,0 +1,115 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cat extends MutualCat
+{
+    private boolean isDown;
+    private int speed = 4;
+    private int speedOnRock = 3;
+    private int counter = 0;
+    private GreenfootImage stand = null;
+    private GreenfootImage run1 = null;
+    private GreenfootImage run2 = null;
+    
+    public Cat() {
+        stand = new GreenfootImage("Stand1.png");
+        run1 = new GreenfootImage("Run1.png");
+        run2 = new GreenfootImage("Run2.png");
+        setImage(stand);
+        isDown = false;
+    }    
+    /**
+     * Act - do whatever the Cat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() {
+        movement();
+        if (isMove()) {
+            switchImage();
+        }
+        else
+            setImage(stand);
+        if (isTouching(Island1.class)) {
+            setLocation(getX(), getY());
+            getWorld().setPaintOrder(Cat.class);
+        }
+        if (isTouching(Rockx.class) && speed != 0) {
+            setLocation(getX(), getY() + speedOnRock);
+            if (isAtEdge())
+                speedOnRock *= -1;
+            getWorld().setPaintOrder(Cat.class);
+        }
+        else if(!isTouching(Island1.class) && !isTouching(Island2.class) && !isTouching(Island3.class) && !isTouching(Land.class) &&
+        !isTouching(Rockx.class)){
+            stand = new GreenfootImage("graycatdrowed2.png");
+            getWorld().setPaintOrder(Rockx.class);
+            speed = 0;
+        }
+        if (isTouching(Star.class)) {
+            removeTouching(Star.class);
+        }
+        if (isTouching(Entrance2.class)) {
+            getWorld().showText("Congrats! Mission Accomplished!", 700, 330);
+            speed = 0;
+            if (Greenfoot.isKeyDown("enter"))
+            Greenfoot.setWorld(new Lvl2());
+        }
+    }    
+
+    /**
+     * checks if the character is moving
+     */
+    public Boolean isMove() {
+        if (speed == 4 && isDown == true)
+            return true;
+        else
+            return false;
+    }
+    
+    public void switchImage() {
+        if (getImage() == run1) {
+            if (counter == 10) {
+                setImage(run2);
+                counter = 0;
+            }
+            else
+            counter += 1;
+        }
+        else {
+            if (counter == 10) {
+                setImage(run1);
+                counter = 0;
+            }
+            else
+                counter += 1;
+            }
+        }
+        
+    public void movement() {
+        if (Greenfoot.isKeyDown("up")) {
+            setLocation(getX(), getY() - speed);
+            isDown = true;
+        }
+        else if (Greenfoot.isKeyDown("down")) {
+            setLocation(getX(), getY() + speed);
+            isDown = true;
+        }
+        else if (Greenfoot.isKeyDown("left")) {
+            setLocation(getX() - speed, getY());
+            isDown = true;
+        }
+        else if (Greenfoot.isKeyDown("right")) {
+            setLocation(getX() + speed, getY());
+            isDown = true;
+        }
+        else
+            isDown = false;
+    }
+            
+}
+    
--- Greenfoot_The Journey of Professeur Cat/Lvl1Temp.java
+++ Greenfoot_The Journey of Professeur Cat/Lvl1Temp.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Lvl1Temp here.
+ * Write a description of class MyWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -10,10 +10,74 @@ public class Lvl1Temp extends MutualWorld
 {
 
     /**
-     * Constructor for objects of class Lvl1Temp.
+     * Constructor for objects of class MyWorld.
      * 
      */
     public Lvl1Temp()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        prepare();
+    }
+    
+     public void act() {
+        if (Greenfoot.isKeyDown("i"))
+            Greenfoot.setWorld(new InstructionPage());        
+        if (Greenfoot.isKeyDown("m"))
+            Greenfoot.setWorld(new AMainMenu());
+        if (Greenfoot.isKeyDown("r"))
+            Greenfoot.setWorld(new Lvl2());                  
+    }    
+
+    public void loseLife(int life) {        
+        if (lifeNum > 0) {
+            loseSound.play();
+            lifeNum -= life;
+            showText("Life: " + lifeNum, 50, 50);  
+        }
+        if (lifeNum > 0) {
+            addObject(new Cat2(), 53,353); 
+        }
+        if (lifeNum < 1) {
+            showText("Game Over\nPress m to return to the main menu", 705, 335);              
+        }
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
     {
+        Island1 island1 = new Island1();
+        addObject(island1,125,161);
+        Rockx rock = new Rockx();
+        addObject(rock,261,167);
+        Island3 island3 = new Island3();
+        addObject(island3,490,320);
+        Rockx rock2 = new Rockx();
+        addObject(rock2,1264,446);
+        Rockx rock3 = new Rockx();
+        addObject(rock3,695,333);
+        Island2 island2 = new Island2();
+        addObject(island2,893,195);
+        removeObject(rock2);
+
+        addObject(rock2,1081,150);
+        Land land = new Land();
+        addObject(land,1439,320);
+        Rockx rock4 = new Rockx();
+        addObject(rock4,1198,500);
+        Rockx rock5 = new Rockx();
+        addObject(rock5,1315,100);
+        Cat cat = new Cat();
+        addObject(cat,122,129);
+        Entrance2 entrance2 = new Entrance2();
+        addObject(entrance2,1444,43);
+        Star star = new Star();
+        addObject(star,536,299);
+        Star star2 = new Star();
+        addObject(star2,904,107);
+        Star star3 = new Star();
+        addObject(star3,1440,443);
     }
 }
