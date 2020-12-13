7823f8b5931d7fae8434330b9f40c824c65bab53
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2020-12-13 00:23:10 -0500

    Update Island3.java

--- Greenfoot_The Journey of Professeur Cat/Island3.java
+++ Greenfoot_The Journey of Professeur Cat/Island3.java
@@ -1,9 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Island3 here.
+ * the class of the island3
  * 
- * @author (your name) 
+ * @author Krikor Astour 
  * @version (a version number or a date)
  */
 public class Island3 extends Actor
